ab8db2e7d31dd8b54c20e69fa194f6d089b316c4
volkangurbuz <volkangurbuz16@gmail.com>
2020-06-15 19:08:57 +0200

    added lombok and result support

---
+++ VerifySystem-Api/src/main/java/com/volkangurbuz/verifysystem/Utilities/Results/Result.java
@@ -0,0 +1,7 @@
+package com.volkangurbuz.verifysystem.Utilities.Results;
+
+public class Result {
+
+  public boolean success;
+  public String message;
+}
---
+++ VerifySystem-Api/src/main/java/com/volkangurbuz/verifysystem/services/VerifyService.java
@@ -0,0 +1,4 @@
+package com.volkangurbuz.verifysystem.services;
+
+public interface VerifyService {
+}
