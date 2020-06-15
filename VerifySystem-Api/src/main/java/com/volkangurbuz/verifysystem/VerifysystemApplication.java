3fbeb993ff1049e912fa557f472efd69dbba5e82
volkangurbuz <volkangurbuz16@gmail.com>
2020-06-15 18:51:13 +0200

    added verifyservice and templates on

---
+++ VerifySystem-Api/src/main/java/com/volkangurbuz/verifysystem/VerifysystemApplication.java
@@ -0,0 +1,12 @@
+package com.volkangurbuz.verifysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifysystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VerifysystemApplication.class, args);
+  }
+}
