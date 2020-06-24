7c57791570ef5f83d3f6241934d1154ffa7adf31
volkangurbuz <volkangurbuz16@gmail.com>
2020-06-24 15:22:53 +0200

    added creation service to build

---
+++ CustomerCreationService/src/main/java/com/volkangurbuz/customercreationservice/CustomercreationserviceApplication.java
@@ -0,0 +1,12 @@
+package com.volkangurbuz.customercreationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomercreationserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CustomercreationserviceApplication.class, args);
+  }
+}
---
+++ CustomerCreationService/src/test/java/com/volkangurbuz/customercreationservice/CustomercreationserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.volkangurbuz.customercreationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomercreationserviceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
